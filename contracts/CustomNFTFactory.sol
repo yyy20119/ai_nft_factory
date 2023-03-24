57a9f3bc399a0ab0f73a9236fb16feebdcd446c5
yyy20119 <55304848+yyy20119@users.noreply.github.com>
2023-03-24 23:56:10 +0800

    Create CustomNFTFactory.sol

---
+++ contracts/CustomNFTFactory.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract CustomNFTFactory {
+    uint256 public contractCounter;
+    mapping(uint256 => address) public contracts;
+    mapping(address => uint256[]) public collections;
+
+    function createNFTContract(string memory _name, string memory _symbol, string memory _uri) public returns (address) {
+        ERC721Custom newContract = new ERC721Custom(_name, _symbol, _uri, msg.sender);
+        contracts[contractCounter] = address(newContract);
+        collections[msg.sender].push(contractCounter);
+        contractCounter++;
+        return address(newContract);
+    }
+}
+
+contract ERC721Custom is ERC721 {
+    string public TOKEN_URI ="";
+
+    constructor(string memory _name, string memory _symbol, string memory _uri, address owner) ERC721(_name, _symbol) {
+        TOKEN_URI = _uri;
+        _safeMint(owner, 0);
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        return TOKEN_URI;
+    }
+}
